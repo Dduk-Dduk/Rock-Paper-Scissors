ef9136ba9af6c86db485a4badcfadd26a0baccbf
Dduk-Dduk <121329652+Dduk-Dduk@users.noreply.github.com>
2025-07-07 15:07:34 -0700

    Make return statement for respective user input

--- myscript.js
+++ myscript.js
@@ -12,4 +12,11 @@ console.log(getComputerChoice())
 
 function getHumanChoice(){
     let number = parseInt(prompt("0 for Rock, 1 for Paper, 2 for Scissors!"))
+    if(number === 0){
+        return "Rock"
+    } else if (number === 1){
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
