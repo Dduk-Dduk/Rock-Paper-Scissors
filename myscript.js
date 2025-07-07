c50a46a63db1825a9d50430b3bed2e242d8f0f86
Dduk-Dduk <121329652+Dduk-Dduk@users.noreply.github.com>
2025-07-07 14:53:41 -0700

    Add paper and scissors conditions and tests

--- myscript.js
+++ myscript.js
@@ -2,5 +2,10 @@ function getComputerChoice() {
     let number = Math.random() * 3
     if (number < 1) {
         console.log("Rock")
+    } else if (number < 2){
+        console.log("Paper")
+    } else {
+        console.log("Scissors")
     }
 }
+getComputerChoice()
